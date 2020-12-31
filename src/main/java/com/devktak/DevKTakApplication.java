589822cd44ca53e3ac3ab545f2191a34a2cc0dbe
박경탁 <qkrrudxkr77@naver.com>
2021-01-01 00:29:39 +0900

    git init

---
+++ src/main/java/com/devktak/DevKTakApplication.java
@@ -0,0 +1,13 @@
+package com.devktak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevKTakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevKTakApplication.class, args);
+    }
+
+}
