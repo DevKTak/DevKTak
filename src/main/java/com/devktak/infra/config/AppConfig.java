aafbe56b03de96b6e56712067e99fced7e791ac2
박경탁 <qkrrudxkr77@naver.com>
2021-10-24 21:44:08 +0900

    로그용 인터셉터 추가

--- src/main/java/com/devktak/infra/config/AppConfig.java
+++ src/main/java/com/devktak/infra/config/AppConfig.java
@@ -1,11 +1,13 @@
 package com.devktak.infra.config;
 
+import com.devktak.infra.interceptor.LogInterceptor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,5 +34,14 @@ public class AppConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/gallery/**")
                 .addResourceLocations("file:///C:/upload/");
+    }
+
+    /** 로그 인터셉터 **/
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+           .order(1)
+           .addPathPatterns("/**")
+           .excludePathPatterns("/css/**", "*.ico", "/error");
     }
 }
