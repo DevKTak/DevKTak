7a4eecca7fbaa0395449960bb5518cec8279392b
박경탁 <qkrrudxkr77@naver.com>
2021-11-29 01:58:28 +0900

    upload 경로 applicatoin.properties 로 빼기

--- src/main/java/com/devktak/infra/config/AppConfig.java
+++ src/main/java/com/devktak/infra/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.devktak.infra.config;
 import com.devktak.infra.interceptor.LogInterceptor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -14,34 +15,44 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
-    @Bean // BCrypt 인코딩을 위해 빈으로 등록
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // BCrypt 해싱 알고리즘 사용
-    }
+    @Value("${resource.path}")
+    private String resourcePath;
 
-    @Bean // ModelMapper 사용을 위해 등록
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT); // 완전히 일치 할 경우에만 매칭 하는 설정
+    @Value("${upload.path")
+    private String uploadPath;
+
+  @Bean // BCrypt 인코딩을 위해 빈으로 등록
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // BCrypt 해싱 알고리즘 사용
+  }
+
+  @Bean // ModelMapper 사용을 위해 등록
+  public ModelMapper modelMapper() {
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.getConfiguration()
+       .setMatchingStrategy(MatchingStrategies.STRICT); // 완전히 일치 할 경우에만 매칭 하는 설정
 //                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE)
 //                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE);
-        return modelMapper;
-    }
+    return modelMapper;
+  }
 
-    /** 외부 경로 매핑 **/
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/gallery/**")
-                .addResourceLocations("file:///C:/upload/");
-    }
+  /**
+   * 외부 경로 매핑
+   **/
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler(uploadPath)
+       .addResourceLocations(resourcePath);
+  }
 
-    /** 로그 인터셉터 **/
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LogInterceptor())
-           .order(1)
-           .addPathPatterns("/**")
-           .excludePathPatterns("/css/**", "*.ico", "/error");
-    }
+  /**
+   * 로그 인터셉터
+   **/
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+       .order(1)
+       .addPathPatterns("/**")
+       .excludePathPatterns("/css/**", "*.ico", "/error");
+  }
 }
