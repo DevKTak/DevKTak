49a2cd4578bb92dfcb104e44dac5bb46b6054d46
ktpark <>
2022-01-27 22:39:19 +0900

    발자취 남기기 디자인 변경

--- src/main/java/com/devktak/infra/kafka/KafkaController.java
+++ src/main/java/com/devktak/infra/kafka/KafkaController.java
@@ -18,8 +18,6 @@ public class KafkaController {
     @PostMapping("/kafka")
     public String sendMessage(@RequestBody Map<String, String> param) {
         String message = param.get("message");
-        log.info("< Controller message = {}", message);
-
         this.producer.sendMessage("작성시간 = " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "\n작성내용 = " + message + "\n");
 
         return "success";
