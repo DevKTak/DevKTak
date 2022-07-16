c4e6cf09c2967306e65be8ec61698e3f2993503a
ktpark <>
2022-07-16 21:12:17 +0900

    설정 변경

--- src/main/java/com/devktak/infra/config/AppConfig.java
+++ src/main/java/com/devktak/infra/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.devktak.infra.config;
 
-import com.devktak.infra.interceptor.LogInterceptor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -48,11 +46,11 @@ public class AppConfig implements WebMvcConfigurer {
   /**
    * 로그 인터셉터
    **/
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(new LogInterceptor())
-       .order(1)
-       .addPathPatterns("/**")
-       .excludePathPatterns("/css/**", "*.ico", "/error");
-  }
+//  @Override
+//  public void addInterceptors(InterceptorRegistry registry) {
+//    registry.addInterceptor(new LogInterceptor())
+//       .order(1)
+//       .addPathPatterns("/**")
+//       .excludePathPatterns("/css/**", "*.ico", "/error");
+//  }
 }
